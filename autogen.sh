3aedf1c91c63aff0e6506dd48a61244909bf83fd
Steve Arnold <nerdboy@gentoo.org>
2017-03-10 15:47:16 -0800

    Make new autotools baseline, move to subdirs, add Makefile.am and configure.ac, populate initial GPL files
    Signed-off-by: Steve Arnold <nerdboy@gentoo.org>

---
+++ autogen.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# you can either set the environment variables AUTOCONF, AUTOHEADER, AUTOMAKE,
+# ACLOCAL, AUTOPOINT and/or LIBTOOLIZE to the right versions, or leave them
+# unset and get the defaults
+
+autoreconf --verbose --force --install --make || {
+ echo 'autogen.sh failed';
+ exit 1;
+}
