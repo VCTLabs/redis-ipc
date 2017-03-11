508480ca0fe7cce5f9d8b8e748f6b91879047c85
nll <nll@vctlabs.com>
2017-03-10 19:23:25 -0800

    a little more client code

--- redis_ipc.py
+++ redis_ipc.py
@@ -5,14 +5,17 @@
 
 # the main feature here is a class which will provide the wanted access
 class redis_client(object):
-    def __init__():
-        pass
+    def __init__(compo):
+        self.component=compo
         
-    def redis_ipc_send_command_blocking(dest,qq,cmd,tmout):
+    def redis_ipc_send_command_blocking(dest,cmd,tmout):
         pass
         
     def redis_ipc_receive_command_blocking(qq,tmout):
         pass
         
     def redis_ipc_send_result(cmd_reply,cmd_result):
+        pass
+        
+    def is_json_msg():   # do we need this???
         pass
