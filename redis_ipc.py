255791385c1274a9eeb822901da710853f1a5755
nll <nll@vctlabs.com>
2017-03-10 18:34:08 -0800

    skeleton of redis client

---
+++ redis_ipc.py
@@ -0,0 +1,18 @@
+# This is a Python module to provide client access to the Redis server
+# it is treated as a library
+
+# globals  ???
+
+# the main feature here is a class which will provide the wanted access
+class redis_client(object):
+    def __init__():
+        pass
+        
+    def redis_ipc_send_command_blocking(dest,qq,cmd,tmout):
+        pass
+        
+    def redis_ipc_receive_command_blocking(qq,tmout):
+        pass
+        
+    def redis_ipc_send_result(cmd_reply,cmd_result):
+        pass
