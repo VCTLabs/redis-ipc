eb6cef590fa946e11fbf863e7556d3ef5ea16702
Steve Arnold <nerdboy@gentoo.org>
2017-03-10 20:34:11 -0800

    Working libtool shared library build (no tests yet, so still a WIP)
    Still not sure if that's what we want...
    
    Signed-off-by: Steve Arnold <nerdboy@gentoo.org>

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,39 @@
-#!/bin/sh
-# you can either set the environment variables AUTOCONF, AUTOHEADER, AUTOMAKE,
-# ACLOCAL, AUTOPOINT and/or LIBTOOLIZE to the right versions, or leave them
-# unset and get the defaults
+#! /bin/sh
 
-autoreconf --verbose --force --install --make || {
- echo 'autogen.sh failed';
- exit 1;
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+THEDIR="`pwd`"
+cd "$srcdir"
+DIE=0
+
+abort () {
+    echo "$1 not found or command failed. Aborting!"
+    exit 1
 }
+
+set -x
+libtoolize --ltdl --force --copy || abort "libtoolize"
+aclocal || abort "aclocal"
+autoheader || abort "autoheader"
+automake --gnu --add-missing --copy || abort "automake"
+autoconf || abort "autoconf"
+
+if test -z "$*"; then
+        echo "I am going to run ./configure with no arguments - if you wish "
+        echo "to pass any to it, please specify them on the $0 command line."
+fi
+
+cd "$THEDIR"
+
+$srcdir/configure "$@" || abort "configure"
+
+set +x
+
+echo "Now type:"
+echo
+echo "make"
+echo "make install"
+echo
+echo "have fun."
+
