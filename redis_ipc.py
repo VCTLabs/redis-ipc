5de696ac5a0d6513dd3f49c6ebe831890eeb770a
nll <nll@vctlabs.com>
2017-03-10 21:26:14 -0800

    a little more client code for redis

--- redis_ipc.py
+++ redis_ipc.py
@@ -1,21 +1,30 @@
 # This is a Python module to provide client access to the Redis server
 # it is treated as a library
 
-# globals  ???
+# global data
+"""
+components
+queues
+"""
+# global functions
+def is_json_msg(m):   # do we need this???
+    pass
 
 # the main feature here is a class which will provide the wanted access
 class redis_client(object):
-    def __init__(compo):
+    def __init__(self,compo):
         self.component=compo
+        self.cfg=self.config(cfg_file=None)
         
-    def redis_ipc_send_command_blocking(dest,cmd,tmout):
+    def redis_ipc_send_command_blocking(self,dest,cmd,tmout):
         pass
         
-    def redis_ipc_receive_command_blocking(qq,tmout):
+    def redis_ipc_receive_command_blocking(self,qq,tmout):
         pass
         
-    def redis_ipc_send_result(cmd_reply,cmd_result):
+    def redis_ipc_send_result(self,cmd_reply,cmd_result):
         pass
         
-    def is_json_msg():   # do we need this???
-        pass
+
+        
+    
