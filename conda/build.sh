2cdb882ee3a178d1d76cc4791717ad16b53f0469
Stephen L Arnold <nerdboy@gentoo.org>
2021-08-29 20:11:56 -0700

    update recipe, add pkg-config to test commands, disable inspect
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- conda/build.sh
+++ conda/build.sh
@@ -2,14 +2,20 @@
 
 set -ex
 
+if [[ "$target_platform" == "osx-64" ]]; then
+    CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_MACOSX_RPATH=ON"
+fi
+
 mkdir build
 cd build
 
 cmake \
     -DRIPC_BUILD_TESTING=OFF \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    ${CMAKE_ARGS} \
     ..
 
 cmake --build . --config RelWithDebInfo -- -j$CPU_COUNT
