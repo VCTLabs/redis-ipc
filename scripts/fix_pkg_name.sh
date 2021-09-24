d36fd5c893a1699f676ea377ef5f9b69c0454a95
Stephen L Arnold <nerdboy@gentoo.org>
2021-09-24 15:59:15 -0700

    fix: dev: refactor coverage generation/reporting, add fix script
    * add autobuild to tox, use lcov => gcovr for report
    * xml seems more compliant, except for pkg name="."
    * add fix_pkg_name.sh and run it in coverage workflow
    
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

---
+++ scripts/fix_pkg_name.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# This fixes package name="" in coverage.xml or another coverage filename
+# as the only optional argument: ./fix_pkg_name.sh other-name.xml
+# We default to grepping pkg name from (python) setup.cfg
+# otherwise you should set the REAL_NAME environment override, eg:
+#
+#    REAL_NAME="re2" ./fix_pkg_name.sh
+#
+# or export it first in your shell env.
+
+set -euo pipefail
+
+failures=0
+trap 'failures=$((failures+1))' ERR
+
+COV_FILE=${1:-coverage.xml}
+REAL_NAME=${REAL_NAME:-""}
+VERBOSE="false"  # set to "true" for extra output
+
+NAME_CHECK=$(grep -o 'name=""' "${COV_FILE}" || true)
+
+[[ -z "$NAME_CHECK" ]] && echo "Nothing to fix ..." && exit 0
+[[ -n $REAL_NAME ]] || REAL_NAME=$(grep ^name setup.cfg | cut -d" " -f3)
+[[ -n $REAL_NAME ]] && sed -i -e "s|name=\"\"|name=\"${REAL_NAME}\"|" $COV_FILE
+[[ -n $REAL_NAME ]] && echo "Replaced \"\" with ${REAL_NAME} in ${COV_FILE} ..."
+
+if ((failures != 0)); then
+    echo "Something went wrong !!!"
+    exit 1
+fi
