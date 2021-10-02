73fd2c8a9d530271504d3d79110933ac550841a0
Stephen L Arnold <nerdboy@gentoo.org>
2021-10-01 22:21:38 -0700

    fix: dev: tweak pre-commit cfg, apply cmake/shell changes
    * yaml checks cough parse error on std conda meta.yaml format
    * cmake-format needs fencing/rulers to mark comments
    * add excludes and fence markers
    * restore missing clang toolchain file
    
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- cmake/FindJSONC.cmake
+++ cmake/FindJSONC.cmake
@@ -1,55 +1,44 @@
+# ~~~
 # This module finds headers and libjson-c library.
 # Results are reported in variables:
 #  JSONC_FOUND           - True if headers and library were found
 #  JSONC_INCLUDE_DIRS    - libjson-c include directories
 #  JSONC_LIBRARIES       - libjson-c library to be linked
+# ~~~
 
-find_path(JSONC_INCLUDE_DIR
+find_path(
+  JSONC_INCLUDE_DIR
   NAMES json-c/json.h
-  HINTS
-    ENV VCPKG_ROOT
-    ENV CONDA_PREFIX
+  HINTS ENV VCPKG_ROOT ENV CONDA_PREFIX
   PATH_SUFFIXES include include/json-c
-  PATHS
-  ~/Library/Frameworks
-  /Library/Frameworks
-  /opt/local
-  /opt
-  /usr
-  /usr/local/
-)
+  PATHS ~/Library/Frameworks /Library/Frameworks /opt/local /opt /usr
+        /usr/local/)
 
-find_library(JSONC_LIBRARY
+find_library(
+  JSONC_LIBRARY
   NAMES json-c libjson-c
-  HINTS
-    ENV VCPKG_ROOT
-    ENV CONDA_PREFIX
+  HINTS ENV VCPKG_ROOT ENV CONDA_PREFIX
   PATH_SUFFIXES lib lib64 lib32
-  PATHS
-  ~/Library/Frameworks
-  /Library/Frameworks
-  /opt/local
-  /opt
-  /usr
-  /usr/local/
-)
+  PATHS ~/Library/Frameworks /Library/Frameworks /opt/local /opt /usr
+        /usr/local/)
 
 mark_as_advanced(JSONC_INCLUDE_DIR JSONC_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(JSONC
-  REQUIRED_VARS JSONC_LIBRARY JSONC_INCLUDE_DIR)
+find_package_handle_standard_args(JSONC REQUIRED_VARS JSONC_LIBRARY
+                                  JSONC_INCLUDE_DIR)
 
 if(JSONC_FOUND)
-  # need if _FOUND guard to allow project to autobuild; can't overwrite imported target even if bad
+  # need if _FOUND guard to allow project to autobuild; can't overwrite imported
+  # target even if bad
   set(JSONC_INCLUDE_DIRS ${JSONC_INCLUDE_DIR})
   set(JSONC_LIBRARIES ${JSONC_LIBRARY})
 
   if(NOT TARGET json-c::json-c)
     add_library(json-c::json-c INTERFACE IMPORTED)
-    set_target_properties(json-c::json-c PROPERTIES
-                          INTERFACE_LINK_LIBRARIES "${JSONC_LIBRARIES}"
-                          INTERFACE_INCLUDE_DIRECTORIES "${JSONC_INCLUDE_DIR}"
-                        )
+    set_target_properties(
+      json-c::json-c
+      PROPERTIES INTERFACE_LINK_LIBRARIES "${JSONC_LIBRARIES}"
+                 INTERFACE_INCLUDE_DIRECTORIES "${JSONC_INCLUDE_DIR}")
   endif()
 endif(JSONC_FOUND)
