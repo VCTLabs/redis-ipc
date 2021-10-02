597a80dcefcc10a6c7532fc840ad6b528e1cec30
Stephen L Arnold <nerdboy@gentoo.org>
2021-10-02 13:25:18 -0700

    chg: dev: add pre-commit bash formatter, apply changes
    * update .pre-commit-config.yaml with prettysh
    * add prettysh to tox -e lint command
    * commit autogen.sh format changes
    
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- autogen.sh
+++ autogen.sh
@@ -22,8 +22,8 @@ automake --gnu --add-missing --copy || abort "automake"
 autoconf || abort "autoconf"
 
 if test -z "$*"; then
-        echo "You still need to run ./configure - if you wish to pass any arguments"
-        echo "to it, please specify them on the configure command line."
+    echo "You still need to run ./configure - if you wish to pass any arguments"
+    echo "to it, please specify them on the configure command line."
 fi
 
 #set +x
