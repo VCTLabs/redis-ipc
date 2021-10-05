c221058f454f74ffb2d53b9fd42da15c58fa5654
Stephen L Arnold <nerdboy@gentoo.org>
2021-10-05 15:43:10 -0700

    new: dev: add cmake module to generate coverage data
    * cmake module replicates makefile.am lcov/genhtml commands
    * tox bionic env updated to generate full coverage
    * move lcov to common deps in smoke workflow
    * note full coverage reqiures 2 passes, one with and one without
      the runtime socket path override (true for both autotools
      and cmake builds)
    * enabling cmake coverage also enables building test binaries;
      both are currently OFF by default
    
      - WITH_COVERAGE enables RIPC_BUILD_TESTING
      - enable RIPC_BUILD_TESTING to build test binaries only
    
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

---
+++ cmake/TestCoverage.cmake
@@ -0,0 +1,41 @@
+set(CMAKE_C_FLAGS "-g -O0 --coverage")
+set(CMAKE_CXX_FLAGS "-g -O0 --coverage")
+set(CMAKE_EXE_LINKER_FLAGS "--coverage")
+set(CMAKE_SHARED_LINKER_FLAGS "--coverage")
+
+set(COVERAGE_OUTPUT_DIR "${CMAKE_SOURCE_DIR}/coverage")
+set(TRACEFILE "${CMAKE_SOURCE_DIR}/coverage.info")
+set(REPORT_DIR "${COVERAGE_OUTPUT_DIR}")
+
+add_custom_command(
+    OUTPUT "${TRACEFILE}" always
+
+    WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
+
+    COMMAND ${CMAKE_COMMAND} -E remove "${TRACEFILE}"
+
+    COMMAND ${CMAKE_COMMAND} -E remove_directory "${COVERAGE_OUTPUT_DIR}"
+
+    COMMAND ${CMAKE_CTEST_COMMAND}  # execute default test suite
+
+    COMMAND lcov
+                --config-file "${CMAKE_SOURCE_DIR}/.lcovrc"
+                --capture
+                --directory "${CMAKE_BINARY_DIR}"
+                --include "${CMAKE_SOURCE_DIR}/src/redis_ipc.c"
+                --include "${CMAKE_SOURCE_DIR}/src/redis_ipc.h"
+                --include "${CMAKE_SOURCE_DIR}/inc/json.hh"
+                --output-file "${TRACEFILE}"
+
+    COMMAND genhtml ${TRACEFILE}
+                --prefix "."
+                --title "${CMAKE_PROJECT_NAME}"
+                --legend --show-details
+                --output-directory ${REPORT_DIR}
+
+    VERBATIM  # for correct handling of wildcards in command line parameters
+)
+
+add_custom_target(cov
+    DEPENDS ${TRACEFILE} always
+)
