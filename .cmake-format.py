a4bf234695001fc9d9e2faa8bcb049bdf8b068f6
Stephen L Arnold <nerdboy@gentoo.org>
2021-10-08 10:10:42 -0700

    chg: dev: set cmake-format intending to 4 and reformat
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- .cmake-format.py
+++ .cmake-format.py
@@ -9,3 +9,6 @@ with section("format"):
 
   # How wide to allow formatted cmake files
   line_width = 80
+
+  # How many spaces to tab for indent
+  tab_size = 4
--- cmake/FindPython.cmake
+++ cmake/FindPython.cmake
@@ -2,32 +2,32 @@ find_package(PythonInterp)
 find_package(PythonLibs)
 
 if(PYTHONINTERP_FOUND)
-  execute_process(
-    COMMAND ${PYTHON_EXECUTABLE} --version
-    ERROR_VARIABLE PYTHON_VERSION_FULL
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
-
-  string(REGEX MATCH "[0-9]+.[0-9]+" PYTHON_VERSION_MAJOR_MINOR
-               "${PYTHON_VERSION_FULL}"
-  )
-  if(UNIX)
-    set(PYTHON_PACKAGES_PATH
-        lib/python${PYTHON_VERSION_MAJOR_MINOR}/site-packages
-        CACHE PATH "Where to install the python packages."
+    execute_process(
+        COMMAND ${PYTHON_EXECUTABLE} --version
+        ERROR_VARIABLE PYTHON_VERSION_FULL
+        OUTPUT_STRIP_TRAILING_WHITESPACE
     )
-  endif()
-  if(WIN32)
-    get_filename_component(
-      PYTHON_PATH
-      "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\${PYTHON_VERSION_MAJOR_MINOR}\\InstallPath]"
-      ABSOLUTE
-      CACHE
-    )
-    set(PYTHON_PACKAGES_PATH "${PYTHON_PATH}/Lib/site-packages")
-  endif()
 
-  if("${PYTHON_VERSION_MAJOR_MINOR}" VERSION_GREATER 2.5)
-    set(PYTHON_EXECUTABLE ${PYTHON_EXECUTABLE} -B)
-  endif()
+    string(REGEX MATCH "[0-9]+.[0-9]+" PYTHON_VERSION_MAJOR_MINOR
+                 "${PYTHON_VERSION_FULL}"
+    )
+    if(UNIX)
+        set(PYTHON_PACKAGES_PATH
+            lib/python${PYTHON_VERSION_MAJOR_MINOR}/site-packages
+            CACHE PATH "Where to install the python packages."
+        )
+    endif()
+    if(WIN32)
+        get_filename_component(
+            PYTHON_PATH
+            "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\${PYTHON_VERSION_MAJOR_MINOR}\\InstallPath]"
+            ABSOLUTE
+            CACHE
+        )
+        set(PYTHON_PACKAGES_PATH "${PYTHON_PATH}/Lib/site-packages")
+    endif()
+
+    if("${PYTHON_VERSION_MAJOR_MINOR}" VERSION_GREATER 2.5)
+        set(PYTHON_EXECUTABLE ${PYTHON_EXECUTABLE} -B)
+    endif()
 endif(PYTHONINTERP_FOUND)
